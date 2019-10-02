a7b7ef54a31211b218fb6ca8fe3a0c72aa271ec1
Monish sharma <sharma.monish17@gmail.com>
2019-10-02 21:09:08 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,3 +100,18 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("activeAccordion");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
